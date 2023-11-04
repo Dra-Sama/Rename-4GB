18aeab894ab3eba091f10ada1861b197eb6256d6
kagutsuchi57 <103054544+kagutsuchi57@users.noreply.github.com>
2023-11-05 04:42:18 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,7 +1,7 @@
 import os 
 from config import *
 from pyrogram import Client, filters
-from pyrogram.types import ParseMode 
+from pyrogram.enums import ParseMode
 token = TOKEN
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
