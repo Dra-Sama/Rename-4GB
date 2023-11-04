5c0a7d1b634cd9528ccbbe0cd3281517b68d899f
kagutsuchi57 <103054544+kagutsuchi57@users.noreply.github.com>
2023-11-05 04:44:46 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -14,13 +14,13 @@ async def start(client, message):
     total_rename = data["total_rename"]
     total_size = data["total_size"]
     await message.reply_text(
-        f"<b>🌟 Total Users:</b> {total_user()}<br>"
-        f"<b>🤖 Bot:</b> <a href='https://t.me/Wizard_Bots'>Wizard Bots</a><br>"
-        f"<b>👤 Creator:</b> <a href='https://t.me/cant_think_1'>Tenjiku</a><br>"
-        f"<b>📚 Language:</b> Python3<br>"
-        f"<b>📦 Library:</b> Pyrogram<br>"
-        f"<b>🖥️ Server:</b> VPS Server<br>"
-        f"<b>📂 Total Renamed Files:</b> {total_rename}<br>"
+        f"<b>🌟 Total Users:</b> {total_user()}\n"
+        f"<b>🤖 Bot:</b> <a href='https://t.me/Wizard_Bots'>Wizard Bots</a>\n"
+        f"<b>👤 Creator:</b> <a href='https://t.me/cant_think_1'>Tenjiku</a>\n"
+        f"<b>📚 Language:</b> Python3\n"
+        f"<b>📦 Library:</b> Pyrogram\n"
+        f"<b>🖥️ Server:</b> VPS Server\n"
+        f"<b>📂 Total Renamed Files:</b> {total_rename}\n"
         f"<b>💽 Total Size Renamed:</b> {humanbytes(int(total_size))}",
         quote=True,
         parse_mode=ParseMode.HTML,
