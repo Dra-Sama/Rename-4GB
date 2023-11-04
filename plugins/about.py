d3419b3ad5600ca5f00accaf4b0c0a51586b3fbe
kagutsuchi57 <103054544+kagutsuchi57@users.noreply.github.com>
2023-11-05 04:12:05 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -11,4 +11,16 @@ async def start(client,message):
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"Total User:- {total_user()}\nBot :- @Wizard_Bots\nCreater :- @cant_think_1\nLanguage :-Python3\nLibrary :- Pyrogram\nServer :- VPS Server\nTotal Renamed File :-{total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} ",quote=True)
+	await message.reply_text(
+    f"🌟 *Total User:* {total_user()}\n"
+    f"🤖 *Bot:* @Wizard_Bots\n"
+    f"👤 *Creator:* @cant_think_1\n"
+    f"📚 *Language:* Python3\n"
+    f"📦 *Library:* Pyrogram\n"
+    f"🖥️ *Server:* VPS Server\n"
+    f"📂 *Total Renamed Files:* {total_rename}\n"
+    f"💽 *Total Size Renamed:* {humanbytes(int(total_size))}",
+    quote=True,
+    parse_mode=ParseMode.MARKDOWN
+)
+
